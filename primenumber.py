6c94683c3865fee191318812438ac2827050a257
aravind172krishna <47302069+aravind172krishna@users.noreply.github.com>
2019-02-04 22:37:06 +0530

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -1,8 +1,10 @@
 num = int(input())  
-  if num <=1000:  
+  
+if num <=1000:  
    for i in range(2,num):  
        if (num % i) == 0:  
            print("no")  
+          
            break  
    else:  
        print("yes")  
